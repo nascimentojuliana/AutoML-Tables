a6e3f195c6e15c8190ad38152a68b8f3ebe4a0d6
Juliana Nascimento <juliacabe@gmail.com>
2020-09-19 15:03:00 -0300

    adiciona run.sh

---
+++ src/run.sh
@@ -0,0 +1,9 @@
+#
+#!/bin/bash
+
+python3 treinamento_modelo_automl.py  --project_id "" \
+	                   --compute_region '' \
+	                   --input '' \
+	                   --bucket '' \
+	                   --model '' \
+	                   --output_path '' \
